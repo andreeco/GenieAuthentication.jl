cbe0098bf30d47d4874c830336b2e9a72c6ecce7
andreeco <andre.herling@outlook.de>
2025-08-14 22:54:36 +0200

    Fix Argon2 wrappers

--- files/app/resources/users/Users.jl
+++ files/app/resources/users/Users.jl
@@ -8,25 +8,26 @@ using Sodium
 export User, hash_password, verify_password
 
 Base.@kwdef mutable struct User <: AbstractModel
-  ### FIELDS
-  id::DbId = DbId()
-  username::String = ""
-  password::String = ""
-  name::String = ""
-  email::String = ""
+    ### FIELDS
+    id::DbId = DbId()
+    username::String = ""
+    password::String = ""
+    name::String = ""
+    email::String = ""
 end
 
 Validation.validator(u::Type{User}) = ModelValidator([
-  ValidationRule(:username, UsersValidator.not_empty),
-  ValidationRule(:username, UsersValidator.unique),
-  ValidationRule(:password, UsersValidator.not_empty),
-  ValidationRule(:email, UsersValidator.not_empty),
-  ValidationRule(:email, UsersValidator.unique),
-  ValidationRule(:name, UsersValidator.not_empty)
+    ValidationRule(:username, UsersValidator.not_empty),
+    ValidationRule(:username, UsersValidator.unique),
+    ValidationRule(:password, UsersValidator.not_empty),
+    ValidationRule(:email, UsersValidator.not_empty),
+    ValidationRule(:email, UsersValidator.unique),
+    ValidationRule(:name, UsersValidator.not_empty)
 ])
 
 const OPSLIMIT = UInt64(Sodium.crypto_pwhash_OPSLIMIT_MODERATE)
 const MEMLIMIT = UInt64(Sodium.crypto_pwhash_MEMLIMIT_MODERATE)
+const STRBYTES = Sodium.crypto_pwhash_STRBYTES
 
 """
     hash_password(password::AbstractString)::String
@@ -51,15 +52,12 @@ false
 
 See also [`GenieAuthentication.Users.verify_password`](@ref).
 """
-function hash_password(password::AbstractString)
-  buf = Vector{UInt8}(undef, Sodium.crypto_pwhash_STRBYTES)
-  status = Sodium.crypto_pwhash_str(
-    buf,
-    password, UInt64(sizeof(password)),
-    OPSLIMIT, MEMLIMIT
-  )
-  @assert status == 0 "crypto_pwhash_str failed"
-  return unsafe_string(Ptr{Cchar}(pointer(buf)))
+function hash_password(password::AbstractString)::String
+    buf = Vector{UInt8}(undef, STRBYTES)
+    status = Sodium.crypto_pwhash_str(
+        buf, password, UInt64(ncodeunits(password)), OPSLIMIT, MEMLIMIT)
+    @assert status == 0 "crypto_pwhash_str failed"
+    return unsafe_string(pointer(buf))
 end
 
 """
@@ -88,12 +86,11 @@ See also [`GenieAuthentication.Users.hash_password `](@ref).
 function verify_password(stored::AbstractString, password::AbstractString)
     if startswith(stored, "\$argon2id\$")
         status = Sodium.crypto_pwhash_str_verify(
-            Vector{UInt8}(stored), password, UInt64(sizeof(password))
+            stored, password, UInt64(ncodeunits(password))
         )
         return status == 0
     else
         return stored == sha256(password)
     end
 end
-
 end
